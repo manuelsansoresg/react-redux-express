41823f779c8d8561aef516080ff401549c0adffa
manuel <manuelsansoresg@gmail.com>
2017-10-02 13:55:40 -0500

    configuration webpack

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+app.use('/static',express.static(__dirname+'/public'));
+
+app.set('views',__dirname+'/views');
+app.set('view engine','pug');
+
+app.get('/', function (req, res) {
+   res.render('index');
+});
+app.listen(3000,function () {
+    console.log('Listening to this join  on port 3000');
+})
